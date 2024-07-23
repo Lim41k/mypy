6c24c351e7886a71df002726ebbe56d0d89f6d87
Lim41k <168574663+Lim41k@users.noreply.github.com>
2024-07-23 18:16:02 +0300

    Create install_docker.sh

---
+++ Dockerfile/install_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Прверка наличия Docker"
+if command -v docker &>/dev/null; then
+echo "Docker установлен"
+docker --version
+else
+    sudo apt update
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    sudo apt update
+    apt-cache policy docker-ce
+    sudo apt install docker-ce
+    docker --version
+fi
+read -p "Введите ваш токен для телеграмм-бота: " token
+echo "TOKEN=\"$token\"" > .env
+wget https://raw.githubusercontent.com/Lim41k/mypy/master/Dockerfile/Dockerfile -O Dockerfile
+docker build -t my_test_bot .
+docker run -d --name my_running_bot my_test_bot
+rm Dockerfile install_docker.sh .env
+
